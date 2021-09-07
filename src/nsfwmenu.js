81d4b96a17c9ba3265429a50f749339a966a3752
Death <85760563+Lalelilolu-2404@users.noreply.github.com>
2021-09-07 15:02:50 +0200

    Add files via upload

---
+++ src/nsfwmenu.js
@@ -0,0 +1,27 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `╭❍ *𝐂𝐨𝐧𝐟𝐮𝐁𝐨𝐭*
+│
+│✎_𝑴𝒆𝒏𝒖 +18_
+│
+│๛ ${prefix}pussy
+│๛ ${prefix}pussyimage
+│๛ ${prefix}solog
+│๛ ${prefix}yuri
+│๛ ${prefix}anal
+│๛ ${prefix}pwankg
+│๛ ${prefix}eron
+│๛ ${prefix}ero
+│๛ ${prefix}erok
+│๛ ${prefix}hentai
+│๛ ${prefix}blowjob
+│๛ ${prefix}boobs
+│๛ ${prefix}H
+│๛ ${prefix}futanari
+│๛ ${prefix}femdom
+│๛ ${prefix}oppai
+│
+╰❏`
+
+}
+
+exports.nsfwmenu = nsfwmenu
